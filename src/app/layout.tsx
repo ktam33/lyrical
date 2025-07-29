db1e2f8b5baae7dc05f8e9b9a6e88fd9f2c27815
Kenneth Tam <ktam33@gmail.com>
2025-07-29 16:26:59 -0500

    package update and icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Cantonese Lyrics Translator",
   description: "Translate Cantonese song lyrics and discover new characters",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎤</text></svg>",
+  },
 };
 
 export default function RootLayout({
